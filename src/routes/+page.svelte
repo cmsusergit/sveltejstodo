a48f6ef0fb7ea318a1a64f2f477762386b75d15d
cmsusergit <45680530+cmsusergit@users.noreply.github.com>
2022-12-16 20:22:08 +0530

    2 files modified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script>
+import { supabase } from '$lib/db'
+
 import {page} from '$app/stores'
+import {Badge} from 'spaper'
+import { onMount } from 'svelte'
+let employeeCount=0,leaveFormCount=0
+const fetchEmployeeCount=async ()=>{
+	let { data, error } = await supabase
+			.rpc('countemployee')
+		if (error) {
+			console.error(error)
+			alert(JSON.stringify(error))
+		
+		}
+		else {
+			employeeCount=data
+		}
+}
+onMount(()=>{
+  fetchEmployeeCount()
+})
 </script>
 <svelte:head>
 	<title>Home</title>
@@ -9,10 +29,21 @@ import {page} from '$app/stores'
 <div class="border">
 	<h4>Hello {$page.data.session.user.email}</h4>
 </div>
+<div style="display: flex;justify-content: space-around;">
+<div class='recordtitle'>
+	<a href="/employee"><h4>Total Employee Records: <Badge>{employeeCount}</Badge></h4></a>
+</div>
+<div class="recordtitle">	
+	<h4>Total Leave Form Records: <Badge>{leaveFormCount}</Badge></h4>
+</div>
+</div>
 </section>
-
 <style>
-div{}
+.recordtitle{
+	background:floralwhite;color:indigo;
+	padding:.2em;
+	margin:1em;width:50%;
+}
 h4{
 	color:indigo;
 	padding:0em .5em 0em .5em;
--- src/routes/employee/+page.svelte
+++ src/routes/employee/+page.svelte
@@ -12,11 +12,30 @@ let loading = false,isOpenDlg=false
 let currPage=1,pageSize=2
 
 let employeeList=[]
-let employeeRecord={}
+let employeeRecord={},recordCount=0
 let isUpdate=false,recordIdToRemove=-1
+$:{
+  if(currPage){
+    fetchEmployee()
+  }
+}
 onMount(()=>{
+  fetchCount()
   fetchEmployee()
 })
+const fetchCount=async()=>{
+  loading=true
+  let { data, error } = await supabase
+    .rpc('countemployee')
+  if (error) {
+    console.error(error)
+    alert(JSON.stringify(error))
+  }
+  else {
+    recordCount=data
+  }
+  loading=false
+}
 const handleAddRecord=()=>{
   employeeRecord={
       user_id:$page.data.session.user.id,
@@ -35,8 +54,6 @@ const handleEdit=(record)=>{
   isUpdate=true
   isOpenDlg=true
 }
-
-
 const handleRemove=async()=>{
   loading=true
   const { data, error } = await supabase
@@ -46,16 +63,30 @@ const handleRemove=async()=>{
   if(error)
     alert(JSON.stringify(error))
   loading=false  
+  fetchCount()
+
+  const nn=recordCount/pageSize
+  if(typeof nn == 'number' && !isNaN(nn))
+    if (!Number.isInteger(nn)) {
+      currPage=(currPage-1)<1?1:(currPage-1)
+    }
   fetchEmployee()
   displayToast('Removed Record','success')
 }
-
 const columnList=[ 'ID','Name','Employee Code','Email','Contact','Department','']
+const onRecordInserted=()=>{
+  displayToast('Added/Updated Record','success')
+  fetchCount()
+  fetchEmployee()
+  isOpenDlg=false
+}
 const fetchEmployee = async () => {
     loading=true;
+    let st=(currPage-1)*pageSize
+    let en=(currPage-1)*pageSize+pageSize-1
     let { data, error } = await supabase
       .from("Employee")
-      .select(`id,emp_name,emp_code,email,contact,emp_type,dept_name,designation`).order("emp_name", { ascending: true });
+      .select(`id,emp_name,emp_code,email,contact,emp_type,dept_name,designation`)).ilike('column', '%CaseInsensitive%').order("emp_name", { ascending: true }.range(st,en);
     if (error) {
       displayToast(JSON.stringify(error),'primary')
       console.error("error", error);
@@ -73,12 +104,26 @@ const fetchEmployee = async () => {
     {/if}
   </div>
 {#if isOpenDlg==true}
-  <AddUpdateRecord employeeRecord={employeeRecord} isUpdate={isUpdate} on:recordadded={()=>{displayToast('Added/Updated Record','success');fetchEmployee();isOpenDlg=false;}} on:closeDlg={()=>isOpenDlg=false}/>
+  <AddUpdateRecord employeeRecord={employeeRecord} isUpdate={isUpdate} on:recordadded={onRecordInserted} on:closeDlg={()=>isOpenDlg=false}/>
 {:else}
   {#if employeeList && employeeList.length>0}
   <div style="margin:.5em;padding-right:0.5em;display:flex;justify-content:flex-end;">
-    <Pagination total={employeeList.length} pageSize={pageSize} bind:current={currPage}/>    
+
+
+
+    <div style="margin-right:0.4em;display:flex;padding:.1em;align-items:center;">
+      <label for="searchText" style="margin-right:.4em;">Search</label>      
+      <input id="searchText" type="text">
+      <select style="margin-left:.2em;">
+        <option value="emp_name">Name</option>
+        <option value="emp_code">Employee Code</option>
+        <option value="email">Email</option>
+        <option value="contact">Contact</option>
+      </select>
+    </div>
+    <Pagination total={recordCount} pageSize={pageSize} bind:current={currPage}/>    
   </div>
+  
   <div class="margin-top-small border" style="width:100%;overflow:auto;">
   <div class="padding-small">
       <table class="table-hover">
@@ -90,10 +135,13 @@ const fetchEmployee = async () => {
           </tr>
         </thead>
         <tbody>
-          {#each employeeList.slice((currPage-1)*pageSize,(currPage-1)*pageSize+pageSize) as record}
+          {#each employeeList as record}
             <tr>
               <td>{record.id}</td>
               <td>{record.emp_name}</td>
+
+
+
               <td>{record.emp_code}</td>
               <td>{record.email}</td>
               <td>{record.contact}</td>
