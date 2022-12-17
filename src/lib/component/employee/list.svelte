a573d0cc9f5766ad8fa666f3e1c41816616821d9
cmsusergit <45680530+cmsusergit@users.noreply.github.com>
2022-12-17 22:50:36 +0530

    4 files modified

--- src/lib/component/employee/list.svelte
+++ src/lib/component/employee/list.svelte
@@ -5,19 +5,16 @@ import { onMount } from 'svelte'
 import { page } from '$app/stores'
 import {Form,Modal,Button,Card,Pagination,Table} from 'spaper'
 import {deptList} from '$lib/../config'
-import Dialog from '$lib/component/dialog.svelte'
-import AddUpdateRecord from '$lib/component/employee/addrecord.svelte'
 import Spinner from '$lib/component/spinner.svelte'
 import {displayToast} from '$lib/../config'
-
-
-let loading = false
+let loading = false,isDetailDlg=false
 let currPage=1,pageSize=4
+
+
 let employeeList=[],searchText=''
 let searchBy='emp_name',selectByDept=''
 let employeeRecord={},recordCount=0
 let sortBy='emp_name',isAscending=true
-let isUpdate=false,recordIdToRemove=-1
 $:{
   if(currPage){
     fetchEmployee()
@@ -46,41 +43,17 @@ const fetchCount=async()=>{
   }
   loading=false
 } 
-const handleAddRecord=()=>{
-  employeeRecord={
-      user_id:$page.data.session.user.id,
-      emp_name:'',
-      designation:'',
-      emp_code:'',
-      emp_type:0, 
-      dept_name:'',
-      contact:'',email:''
-  }
-  isUpdate=false
-}
-const handleEdit=(record)=>{
-  employeeRecord=record
-  isUpdate=true
-}
-const handleRemove=async()=>{
-  loading=true
-  const { data, error } = await supabase
-  .from('Employee')
-  .delete()
-  .eq('id', recordIdToRemove)   
-  if(error)
-    alert(JSON.stringify(error))
-  loading=false  
+
+export const handleRemove=async()=>{
   fetchCount()
+
   const nn=recordCount/pageSize
   if(typeof nn == 'number' && !isNaN(nn))
     if (!Number.isInteger(nn)) {
       currPage=(currPage-1)<1?1:(currPage-1)
     }
   fetchEmployee()
-  displayToast('Removed Record','success')
 }
-
 const columnList=[ {name:'ID',field:'id'},{name:'Name',field:'emp_name'},{name:'Employee Code',field:'emp_code'},{name:'Email',field:'email'},{name:'Contact',field:'contact'},{name:'Department'},{name:''}]
 
 const fetchEmployee = async () => {
@@ -101,16 +74,20 @@ const fetchEmployee = async () => {
     }
     loading=false
   };
+const handleDetail=(record)=>{
+  employeeRecord=record
+  isDetailDlg=true
+}
 </script>
 <div>
-<div style="margin:.5em;padding-right:0.5em;display:flex;justify-content:flex-end;">
-  <div style="margin-right:0.4em;display:flex;padding:.1em;align-items:center;">
+<div style="margin:.5em;padding-right:0.5em;display:flex;justify-content:flex-end;flex-wrap: wrap">
+  <div style="margin-right:0.4em;display:flex;padding:.1em;align-items:center;flex-wrap: wrap">
   <label style="margin-right:.4em;">Select Department</label>     
-
     <select bind:value={selectByDept} style="margin-left:.2em;">
       <option value="">ALL</option>
       {#each deptList as dept}
         <option value={dept.deptName}>{dept.deptName}</option>
+
       {/each}
     </select>
     <div style="margin-left:.2em;margin-right:0.2em;"></div>
@@ -159,19 +136,14 @@ const fetchEmployee = async () => {
             <td>{record.email}</td>
             <td>{record.contact}</td>
             <td>{record.dept_name}</td>
-            
-            
-            
-            
-            
-            
-            
-            
             <td>
-            <slot>
-              <button on:click={()=>handleEdit(record)} class="btn-secondary" style="padding:0.2em;">Edit</button>
-              <button on:click={()=>{recordIdToRemove=record.id}} class="btn-danger" style="padding:0.2em;">Remove</button>
+            <slot currRecord={record}>
             </slot>
+              <button on:click={()=>handleDetail(record)} class="btn-primary" style="padding:0.2em;">
+              <svg class="svg-icon" viewBox="0 0 20 20">
+							<path d="M10,6.978c-1.666,0-3.022,1.356-3.022,3.022S8.334,13.022,10,13.022s3.022-1.356,3.022-3.022S11.666,6.978,10,6.978M10,12.267c-1.25,0-2.267-1.017-2.267-2.267c0-1.25,1.016-2.267,2.267-2.267c1.251,0,2.267,1.016,2.267,2.267C12.267,11.25,11.251,12.267,10,12.267 M18.391,9.733l-1.624-1.639C14.966,6.279,12.563,5.278,10,5.278S5.034,6.279,3.234,8.094L1.609,9.733c-0.146,0.147-0.146,0.386,0,0.533l1.625,1.639c1.8,1.815,4.203,2.816,6.766,2.816s4.966-1.001,6.767-2.816l1.624-1.639C18.536,10.119,18.536,9.881,18.391,9.733 M16.229,11.373c-1.656,1.672-3.868,2.594-6.229,2.594s-4.573-0.922-6.23-2.594L2.41,10l1.36-1.374C5.427,6.955,7.639,6.033,10,6.033s4.573,0.922,6.229,2.593L17.59,10L16.229,11.373z"></path>
+						</svg>
+              </button>
             </td>
           </tr>
         {/each}
@@ -188,14 +160,19 @@ const fetchEmployee = async () => {
 </div>
 {/if}
 </div>
-{#if loading}<div><Spinner/></div>{/if}
-{#if recordIdToRemove!=-1}
+
+
+
+
+
+
+
+
+<Modal bind:active={isDetailDlg}>
 <div>
-  <Dialog on:continue={()=>{handleRemove(recordIdToRemove);recordIdToRemove=-1}} on:close={()=>{recordIdToRemove=-1}} title='Remove Dialog'>  
-    Do You Really Want to Remove?
-  </Dialog>
+  <h4>
+    {JSON.stringify(employeeRecord)}
+  </h4>
 </div>
-{/if}
-
-
- 
+</Modal>
+{#if loading}<div><Spinner/></div>{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,14 @@ import { supabase } from '$lib/db'
 import {page} from '$app/stores'
 import {Badge} from 'spaper'
 import { onMount } from 'svelte'
+import EmployeeComponent from '$lib/component/employee/list.svelte'
 let employeeCount=0,leaveFormCount=0
 const fetchEmployeeCount=async ()=>{
 	let { data, error } = await supabase
-			.rpc('countemployee',{searchby:'emp_name',searchtext:''})
+			.rpc('countemployee',{searchby:'emp_name',searchtext:'',dd:'',deptname:''})
 		if (error) {
+
 			console.error(error)
-			alert(JSON.stringify(error))
-		
 		}
 		else {
 			employeeCount=data
@@ -29,20 +29,26 @@ onMount(()=>{
 <div class="border">
 	<h4>Hello {$page.data.session.user.email}</h4>
 </div>
-<div style="display: flex;justify-content: space-around;">
-<div class='recordtitle'>
-	<a href="/employee"><h4>Total Employee Records: <Badge>{employeeCount}</Badge></h4></a>
+<div style="display: flex;">
+<div class='recordtitle border' style="padding:0px;text-align:center">
+	<a href="/employee">
+		<h4>Total Employee Records: <Badge>{employeeCount}</Badge></h4>
+	</a>
 </div>
-<div class="recordtitle">	
+<div class="recordtitle border" style="padding:0px;text-align:center">	
 	<h4>Total Leave Form Records: <Badge>{leaveFormCount}</Badge></h4>
 </div>
+</div>
+<div class="border padding-large">
+	<EmployeeComponent/>
 </div>
 </section>
 <style>
 .recordtitle{
-	background:floralwhite;color:indigo;
-	padding:.2em;
-	margin:1em;width:50%;
+	background:rgb(244,244,255,1);color:indigo;
+
+	padding:0.2em;
+	margin:.5em;width:50%;
 }
 h4{
 	color:indigo;
