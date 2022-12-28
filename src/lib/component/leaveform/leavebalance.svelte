d441424dab6abb275e42423b63aee399e5486181
cmsusergit <45680530+cmsusergit@users.noreply.github.com>
2022-12-28 20:23:02 +0530

    1 file modified

--- src/lib/component/leaveform/leavebalance.svelte
+++ src/lib/component/leaveform/leavebalance.svelte
@@ -5,7 +5,7 @@
   import {leaveBalance,leaveTypeList} from '$lib/../store'
   import Spinner from '$lib/component/spinner.svelte'
   let loading = false,isOpenDlg=false
-  export let employee_id=-1
+  export let employee_id=-1,is_editable=true
   $:{
     if(employee_id)
       fetchLeaveBalanceByEmpId()
@@ -28,30 +28,52 @@
     if(error){
       alert(JSON.stringify(error))
     }
-    $leaveBalance=data
+    $leaveBalance=[]
+    if($leaveTypeList){
+      $leaveTypeList.forEach(leaveType=>{
+          if(leaveType.is_credit_required==true){
+            const dt=data?.find(ob=>ob.leave_type==leaveType.id)
+            $leaveBalance.push(dt)
+          }    
+      })
+    }
     loading=false
   }
 </script>
-
-
-
-
-
 <div>
-  <table class="border padding-small" style="width:100%;text-align:center;">
+  <p>{JSON.stringify($leaveBalance)}</p>
+  <div class="border padding-small">
+  <table class="padding-small" style="width:100%;text-align:center;">
       <tr>
       {#each $leaveTypeList as leaveType}
           {#if leaveType.is_credit_required==true}<th>{leaveType.leave_alias}</th>{/if}
       {/each}
       </tr>
     <tbody>
-      <tr>      
-
-     {#each $leaveTypeList as leaveType}
-        {#if leaveType.is_credit_required==true}<td>{$leaveBalance?.find(ob=>ob.leave_type==leaveType.id)?.balance}</td>{/if}
+      <tr>   
+      {#each $leaveBalance as lbRecord}
+          <td class="">          
+            {#if is_editable}
+              <div style="display:flex;justify-content:center;">
+                <input bind:value={lbRecord.balance} step="0.5" type="number" min="0" style="width:5em;">
+      
+              </div>
+            {:else}
+              <span>{lbRecord.balance}</span>
+            {/if}
+          </td>        
       {/each}
       </tr>
     </tbody>
+  
   </table>
+  {#if is_editable}
+    <div style="display:flex;justify-content:right;padding-right:5em;padding:5px;">
+      <button>Update</button><button>Close</button>
+    </div>
+  {/if}
+</div>
+
 </div>
 {#if loading}<div><Spinner/></div>{/if}
+
